dd889492ecce87189ba3f7ee5387e17a7df998b6
Yusuf <Yusuf>
2016-09-25 12:56:36 -0400

    I wrote a solution to problem 1

---
+++ 01-IfStatements/Grade Converter.py
@@ -0,0 +1,12 @@
+grade = int(input ('Enter your grade: '))
+if grade >=80:
+    print ("You got an A")
+elif grade >= 70:
+    print ("You got a B")
+elif grade >= 60:
+    print ("You got a C")
+elif grade >= 50:
+    print ("You got a D")
+else:
+    print ("You failed!")
+    
